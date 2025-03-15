c061d8eb9fb831b8c8acff11f14ed29a70512d61
Tomás Atanasoff <tomyatana@gmail.com>
2025-03-15 00:15:12 -0300

    Learnt how to export a zig module lol

--- build.zig
+++ build.zig
@@ -1,14 +1,26 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const bass_zig = b.addModule("bass-zig", .{
-        .root_source_file = b.path("src/bass.zig"),
+    const src_path = b.path("src/bass.zig").src_path;
+    _ = b.addModule("bass-zig", .{
+        .link_libc = true,
+        .root_source_file = .{ .src_path = src_path },
+        .optimize = optimize,
+        .target = target,
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "bass",
+        .root_source_file = .{ .src_path = src_path },
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    bass_zig.linkSystemLibrary("bass", .{ .needed = true });
+
+    lib.linkSystemLibrary2("bass", .{ .needed = true });
+
+    b.installArtifact(lib);
 }
